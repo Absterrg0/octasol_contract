d1777834f61a58ca664f2ff1769fd0f33a5fa81c
ritikbhatt20 <ritikbhatt020@gmail.com>
2025-01-28 13:31:34 +0000

    feat: added bounty state

--- programs/octasol_contract/src/lib.rs
+++ programs/octasol_contract/src/lib.rs
@@ -14,3 +14,35 @@ pub mod octasol_contract {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Bounty {
+    pub maintainer: Pubkey,
+    pub contributor: Option<Pubkey>,
+    pub amount: u64,
+    pub state: BountyState,
+    pub bounty_id: u64,
+    pub github_issue_id: u64,
+    pub maintainer_github_id: u64,
+    pub contributor_github_id: Option<u64>,
+}
+
+impl Bounty {
+    pub const LEN: usize = 8 + // discriminator
+        32 + // maintainer pubkey
+        33 + // contributor option pubkey
+        8 + // amount
+        1 + // state
+        8 + // bounty_id
+        8 + // github_issue_id
+        8 + // maintainer_github_id
+        9; // contributor_github_id option
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq)]
+pub enum BountyState {
+    Created,
+    InProgress,
+    Completed,
+    Cancelled,
+}
